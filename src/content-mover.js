075e39ecff8d29391dc4a4e8e141660456f302f9
minixz <minikuz@gmail.com>
2019-10-06 19:28:50 +0800

    Update background and add cake

--- src/content-mover.js
+++ src/content-mover.js
@@ -1,5 +1,6 @@
 export function start() {
   const content = document.querySelector('#content');
+  const stars = document.querySelectorAll('.stars');
 
   window.addEventListener('mousemove', (ev) => {
     const windowWidth = window.innerWidth;
@@ -7,5 +8,9 @@ export function start() {
     const xPerc = -((ev.clientY - windowHeight / 2) / (windowHeight / 2));
     const yPerc = (ev.clientX - windowWidth / 2) / (windowWidth / 2);
     content.style.transform = `rotateX(${xPerc * 7}deg) rotateY(${yPerc * 10}deg)`;
+
+    stars.forEach((star) => {
+      star.style.backgroundPosition = `${yPerc * 28}px ${-xPerc * 40}px`;
+    });
   });
 }
